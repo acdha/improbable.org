c3b8e071a77ddb06a73d374d750714992bcffc19
Chris Adams <chris@improbable.org>
2010-01-30 15:30:11 -0500

    Gave up on django_compressor, again
    yui-compressor via Makefile is easier and has less to break

--- local_settings.py
+++ local_settings.py
@@ -4,16 +4,12 @@ from django.conf import settings
 import os
 from urlparse import urljoin
 
-# TODO: Smart calculation of cache-buster for media
-
+LOCAL_DEV       = True
 DEBUG           = False
 TEMPLATE_DEBUG  = DEBUG
-COMPRESS        = False
-LOCAL_DEV       = True
-THUMBNAIL_DEBUG = DEBUG #sorl-thumbnail
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME   = os.path.join(os.path.dirname(__file__), 'dev.db')
+#sorl-thumbnail
+THUMBNAIL_DEBUG = DEBUG
 
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 STATIC_ROOT= os.path.join(MEDIA_ROOT, "static")
@@ -34,25 +30,46 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME   = os.path.join(os.path.dirname(__file__), 'dev.db')
+
 EMAIL_HOST          = '127.0.0.1'
 EMAIL_PORT          = 25
 EMAIL_HOST_USER     = ''
 EMAIL_HOST_PASSWORD = ''
 
-CACHE_BACKEND                   = 'dummy://'
-CACHE_MIDDLEWARE_SECONDS        = 60 * 10
+CACHE_BACKEND                   = 'locmem://'
+CACHE_MIDDLEWARE_SECONDS        = 60 * 5
 CACHE_MIDDLEWARE_KEY_PREFIX     = 'improbable.org.'
 CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
 
+INSTALLED_APPS = tuple(app for app in settings.INSTALLED_APPS if app not in ('request', 'compressor'))
+TEMPLATE_CONTEXT_PROCESSORS = settings.TEMPLATE_CONTEXT_PROCESSORS + ('sugar.context_processors.site_settings',)
+
+# NOTE: Your IP must be in this file to use django-debug-toolbar:
+INTERNAL_IPS = ('127.0.0.1',)
+
+# django-debug-toolbar
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
-    'HIDE_DJANGO_SQL': True,
 }
 
-### django-markup
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+# django-markup
 MARKUP_CHOICES = (
     'none',
-    #'markdown',
+    'markdown',
+    'textile',
     'restructuredtext'
 )
 
